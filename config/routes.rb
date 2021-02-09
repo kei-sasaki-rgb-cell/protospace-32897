f3145a51d0f8446e129c2eada20eaf4e2d626e4e
kei-sasaki-rgb-cell <11eftx2tree@gmail.com>
2021-02-09 20:45:06 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "prototypes#index"
+  resources :users, only: [:index, :new, :create, :show]
+  resources :prototypes, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+    resources :comments, only: :create
+  end
 end
